2a9cd381208791dea6406c4e1f0cc59a074faa37
Simone <info@simonepozzobon.com>
2019-10-07 18:43:57 +0200

    Creazione della tabella

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ const mix = require('laravel-mix')
  */
 
 mix
-    .js('resources/js/admin.js', 'public/js/admin.js')
+    .js('resources/js/admin/admin.js', 'public/js/admin.js')
     .js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .webpackConfig(webpack => {
